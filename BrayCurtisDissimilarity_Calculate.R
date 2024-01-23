5f78208b23d6a4a781578156cff3ebb0677bee4f
Tak Tsun (Edmund) Lo <104346689+taktsun@users.noreply.github.com>
2024-01-23 11:01:41 +0100

    Update BrayCurtisDissimilarity_Calculate.R
    A few updates:
    1. removed the error handling on duplicated observations/beeps because they should be done outside of this function
    2. added the "multiplyby" parameter, which allows users to multiply the dissimilarity value by a certain factor. Default to be 1 (no scaling).
    3. added the "addby" parameter, which allows users to add a small constant (e.g., 0.001) to the raw data so that two observations with zero in every variable will return value 0 (no dissimilarity)
    4. applied the bSunarm parameter to observation-level amm full index - previously it's only on the subcomponents

--- BrayCurtisDissimilarity_Calculate.R
+++ BrayCurtisDissimilarity_Calculate.R
@@ -2,68 +2,74 @@
 # Function that calculates Bray-Curtis dissimilarity for ESM data
 # ===============================================================
 library(betapart)
-calcBrayCurtisESM <- function (d, vn, pid, tid, bSubnarm = TRUE, bPersonnarm = TRUE){
+calcBrayCurtisESM <- function (d, vn, pid, tid, bSubnarm = TRUE, bPersonnarm = TRUE, multiplyby = 1, addby = 0.001){
   m<- d[,vn]
-  if (any (table(d[,c(pid,tid)]) >1)){
-    stop("There are duplicated observations/beeps in some of the groups(individuals). Calculation halted.")
-  }
   # T/F values for each observation
-  b_firstbeep<-as.vector(d[tid]==ave(d[tid], d[pid], FUN = min))
-  d$b_firstbeep <- b_firstbeep
+  d$b_firstbeep <- as.logical(d[tid]==ave(d[tid], d[pid], FUN = min))
   d$b_completeER <- complete.cases(m)
 
   # Calculate Bray-Curtis dissimilarity (successive difference)
   d$BrayCurtisFull.suc <-NA
   d$BrayCurtisRepl.suc <-NA
   d$BrayCurtisNest.suc <-NA
-
+  
   # calculating by loop on pairwise dissimilarity because calculating dissimilarity for large matrix takes a long time
   for (i in 1:nrow(d)){
     if(!d$b_firstbeep[i]){ # first beep of each person remains NA
-      tempmat <- rbind(d[i,vn],setNames(d[i-1,vn],names(d[i,vn])))
+      tempmat <- rbind(d[i,vn],setNames(d[i-1,vn],names(d[i,vn]))) + addby 
+      # add a small constant so that two observations with zero in every variable will return value 0 (no dissimilarity)
+      
       tempres <- bray.part(tempmat)
       d$BrayCurtisFull.suc[i] <- tempres$bray[1]
       d$BrayCurtisRepl.suc[i] <- tempres$bray.bal[1]
       d$BrayCurtisNest.suc[i] <- tempres$bray.gra[1]
     }
   }
-
   # Calculate Bray-Curtis dissimilarity (all-moment comparison)
-
   # exclude rows with all NA or only 1 available score, because bray.part needs at least 2 variables for calculation
   dTemp <- d[rowSums(is.na(d[,vn])) < (ncol(d[,vn])-1),
              append(c(pid,tid),vn)] #include only ER items and binding info
-  # remove groups (individuals) with only 1 row left
-  dTemp <- dTemp[ave(dTemp[tid], dTemp[pid], FUN = nrow) > 1, ]
   
   vbray.full <- NULL
   vbray.repl <- NULL
   vbray.nest <- NULL
-  for (i in 1:nrow(unique(dTemp[pid]))){
-    dPerson <- dTemp[dTemp[pid]==unlist(unique(dTemp[pid]))[[i]],] # create a temp d for each person
-    if(is.unsorted(unlist(as.vector(dPerson[tid])))){
-      stop(paste0("Group(participant) ",dPerson[1,pid]," has unsorted observations/beeps. Calculation halted."))
-    }
-    matx <- dPerson[,vn]
+    for (i in 1:length(unique(dTemp[,pid]))){
+      dPerson <- dTemp[dTemp[,pid]==unique(dTemp[,pid])[i],] # create a temp d for each person
+    
+    
+      # add a small constant so that two observations with zero in every variable will return value 0 (no dissimilarity)
+      matx <- dPerson[,vn]+ addby 
+    
     nobs <- nrow(dPerson)
     resbray <- bray.part(matx)
-    vtemp <- colMeans(as.matrix(resbray$bray))
-    vtemp <- ifelse(bSubnarm & is.na(as.matrix(resbray$bray)[,1]),as.matrix(resbray$bray)[,1],vtemp)
-    vbray.full <- append(vbray.full,vtemp*nobs/(nobs-1))
-    vtemp <- colMeans(as.matrix(resbray$bray.bal),na.rm=bSubnarm)
-    vtemp <- ifelse(bSubnarm & is.na(as.matrix(resbray$bray.bal)[,1]),as.matrix(resbray$bray.bal)[,1],vtemp)
-    vbray.repl <- append(vbray.repl,vtemp*nobs/(nobs-1))
-    vtemp <- colMeans(as.matrix(resbray$bray.gra),na.rm=bSubnarm)
-    vtemp <- ifelse(bSubnarm & is.na(as.matrix(resbray$bray.gra)[,1]),as.matrix(resbray$bray.gra)[,1],vtemp)
-    vbray.nest <- append(vbray.nest,vtemp*nobs/(nobs-1))
+    if (nobs ==1){
+      vbray.full <- append(vbray.full,NA)
+      vbray.repl <- append(vbray.repl,NA)
+      vbray.nest <- append(vbray.nest,NA)
+    }else{
+      vtemp <- colMeans(as.matrix(resbray$bray),na.rm=bSubnarm)
+      vtemp <- ifelse(bSubnarm & is.na(as.matrix(resbray$bray)[,1]),as.matrix(resbray$bray)[,1],vtemp)
+      vbray.full <- append(vbray.full,vtemp*nobs/(nobs-1))
+      vtemp <- colMeans(as.matrix(resbray$bray.bal),na.rm=bSubnarm)
+      vtemp <- ifelse(bSubnarm & is.na(as.matrix(resbray$bray.bal)[,1]),as.matrix(resbray$bray.bal)[,1],vtemp)
+      vbray.repl <- append(vbray.repl,vtemp*nobs/(nobs-1))
+      vtemp <- colMeans(as.matrix(resbray$bray.gra),na.rm=bSubnarm)
+      vtemp <- ifelse(bSubnarm & is.na(as.matrix(resbray$bray.gra)[,1]),as.matrix(resbray$bray.gra)[,1],vtemp)
+      vbray.nest <- append(vbray.nest,vtemp*nobs/(nobs-1))
+    }
   }
 
-  dTemp$BrayCurtisFull.amm <- vbray.full
-  dTemp$BrayCurtisRepl.amm <- vbray.repl
-  dTemp$BrayCurtisNest.amm <- vbray.nest
+  dTemp$BrayCurtisFull.amm <- vbray.full * multiplyby
+  dTemp$BrayCurtisRepl.amm <- vbray.repl * multiplyby
+  dTemp$BrayCurtisNest.amm <- vbray.nest * multiplyby
 
   dTemp<- dTemp[,setdiff(names(dTemp), vn)] # remove vn cols
   d<- merge(d,dTemp, by=c(pid,tid),all=TRUE)
+  
+  d$BrayCurtisFull.suc <-d$BrayCurtisFull.suc * multiplyby
+  d$BrayCurtisRepl.suc <- d$BrayCurtisRepl.suc* multiplyby
+  d$BrayCurtisNest.suc <- d$BrayCurtisNest.suc* multiplyby
+
 
   # calculate person-mean of dissimilarity
   d$person_BrayCurtisFull.suc <- ave(d$BrayCurtisFull.suc, d[pid], FUN = function(x) mean(x, na.rm = bPersonnarm))
